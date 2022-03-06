8dc0548fc6f90f5cb2e112fde89def0dedea76ab
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-06 11:10:56 +0100

    initial load

--- config.tf
+++ config.tf
@@ -10,6 +10,37 @@ terraform {
   }
 }
 
+resource "random_string" "autonomous_database_admin_password" {
+  length      = 16
+  min_numeric = 1
+  min_lower   = 1
+  min_upper   = 1
+  min_special = 1
+}
+
+data "oci_database_autonomous_db_versions" "test_autonomous_db_versions" {
+  #Required
+  compartment_id = var.compartment_ocid
+
+  #Optional
+  db_workload = var.autonomous_database_db_workload
+
+  filter {
+    name   = "version"
+    values = ["19c"]
+  }
+}
+
+data "oci_database_autonomous_databases" "autonomous_databases" {
+  #Required
+  compartment_id = var.compartment_ocid
+
+  #Optional
+  display_name = oci_database_autonomous_database.autonomous_database.display_name
+  db_workload  = var.autonomous_database_db_workload
+}
+
+
 locals {}
 
 // Define the wait state for the data requests
--- output.tf
+++ output.tf
@@ -1,2 +1,19 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+
+output "autonomous_database_admin_password" {
+  value = random_string.autonomous_database_admin_password.result
+}
+
+output "autonomous_database_high_connection_string" {
+  value = lookup(
+    oci_database_autonomous_database.autonomous_database.connection_strings[0],
+    "high",
+    "unavailable",
+  )
+}
+
+output "autonomous_databases" {
+  value = data.oci_database_autonomous_databases.autonomous_databases.autonomous_databases
+}
