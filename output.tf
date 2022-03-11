146c786c83bf0e9806377830ffb15bb6e59a645d
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-11 07:57:32 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,14 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-output "autonomous_database_admin_password" {
+output "admin_password" {
   value = var.input.password == false ? random_string.admin_password.result : random_string.admin_password.result
 }
 
-output "autonomous_database_high_connection_string" {
-  value = oci_database_autonomous_database.database[0].connection_strings[0]
+output "connection_strings" {
+  value = oci_database_autonomous_database.database[0].connection_strings[0].all_connection_strings
 }
 
-/*
 output "databases" {
-  value = data.oci_database_autonomous_databases.database
+  value = data.oci_database_autonomous_databases.database[0]
 }
-*/
