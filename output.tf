348aa7dfd1fe2246a48a4d597fdb398aaa0bf1bb
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-10 22:53:31 +0200

    admin password

--- output.tf
+++ output.tf
@@ -1,11 +1,6 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-output "admin_password" {
-  value = var.input.service.stage < 2 ? var.assets.encryption.passwords[var.input.database.password] : base64decode(data.oci_secrets_secretbundle.database.secret_bundle_content.0.content)
-}
-
-/*
 output "database_ids" {
   description = "A list of automous databases created by the database module"
   value       = length(oci_database_autonomous_database.database) > 0 ? {for adb in data.oci_database_autonomous_databases.database.autonomous_databases : adb.display_name => adb.id} : null
@@ -26,4 +21,3 @@ output "connection_urls" {
 output "password" {
   value = length(oci_database_autonomous_database.database) > 0 ? var.assets.encryption.passwords[var.input.database.password] : null
 }
-*/
--- resources.tf
+++ resources.tf
@@ -1,14 +1,13 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-/*
 resource "oci_database_autonomous_database" "database" {
   compartment_id           = data.oci_identity_compartments.database.compartments[0].id
   count                    = local.adb_count
   cpu_core_count           = var.input.database.cores
   data_storage_size_in_tbs = var.input.database.storage
   db_name                  = var.input.database.name
-  admin_password           = var.input.service.stage < 2 ? var.assets.encryption.passwords[var.input.database.password] : data.oci_secrets_secretbundle.database.id
+  admin_password           = var.input.service.stage < 2 ? var.assets.encryption.passwords[var.input.database.password] : base64decode(data.oci_secrets_secretbundle.database.secret_bundle_content.0.content)
   db_version               = var.input.database.version
   display_name             = var.input.database.display_name
   db_workload              = var.input.database.type
@@ -17,4 +16,3 @@ resource "oci_database_autonomous_database" "database" {
   defined_tags             = var.assets.resident.defined_tags
   freeform_tags            = var.assets.resident.freeform_tags
 }
-*/
