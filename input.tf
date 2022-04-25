0dfd7e03f3830c89cda7dc04d9f099efaab804e1
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-04-25 18:05:54 +0200

    Update input.tf

--- input.tf
+++ input.tf
@@ -2,33 +2,34 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 variable "account" {
+  description = "Account parameter"
   type = object({
     tenancy_id = string,
     class      = bool
   })
-  description = "Account parameter"
 }
 
 variable "options" {
+  description = "Input for database module"
   type = object({
     create   = bool,
     password = string
   })
-  description = "Input for database module"
 }
 
 variable "configuration" {
+  description = "configuration parameter for the database service"
   type = object({
-    service  = any,
+    resident  = any,
     database = any
   })
 }
 
 variable "assets" {
+  description = "Retrieve asset identifier"
   type = object({
     encryption = any,
     network    = any,
     resident   = any
   })
-  description = "Retrieve asset identifier"
 }
